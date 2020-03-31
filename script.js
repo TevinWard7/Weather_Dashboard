775c0f46aab4ff2bac0a9e9501a255fb94808f43
Tevin <tevinward7@gmail.com>
2020-03-31 18:16:08 -0400

    made uv index of city chosen dislay

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ $(document).ready(function () {
 
     $("#find").on("click", function (event) {
         var city = $("#city").val().trim();
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2f4e8dfb57bfb030fdf2188d7ed0b5e7"
+        var apiKey = "2f4e8dfb57bfb030fdf2188d7ed0b5e7"
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2f4e8dfb57bfb030fdf2188d7ed0b5e7";
 
         $.ajax({
             url: queryURL,
@@ -17,6 +18,27 @@ $(document).ready(function () {
             $("#temperature").text(response.main.temp);
             $("#humidity").text(response.main.humidity);
             $("#wind-speed").text(response.wind.speed);
+
+            var lon = response.coord.lon;
+            var lat = response.coord.lat;
+            var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+
+            $.ajax({
+                url: uvURL,
+                method: "GET"
+            }).then(function (response) {
+                $("#uv-index").text(response.value);
+
+                var forecastURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+                $.ajax({
+                    url: forecastURL,
+                    method: "GET"
+                }).then(function (response) {
+                    console.log(response)
+                    $("#forecast").text();
+                });
+            });
 
 
         });
