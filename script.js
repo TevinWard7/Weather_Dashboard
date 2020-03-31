130b0a9b4e24987a3104c51d784fb5feaa4ca189
Tevin <tevinward7@gmail.com>
2020-03-31 19:37:30 -0400

    got dates of 5 day forecast to show

--- script.js
+++ script.js
@@ -29,14 +29,18 @@ $(document).ready(function () {
             }).then(function (response) {
                 $("#uv-index").text(response.value);
 
-                var forecastURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+                var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=2f4e8dfb57bfb030fdf2188d7ed0b5e7"
 
                 $.ajax({
                     url: forecastURL,
                     method: "GET"
                 }).then(function (response) {
                     console.log(response)
-                    $("#forecast").text();
+                    $("#forecastday1").text(response.list[0].dt_txt);
+                    $("#forecastday2").text(response.list[1].dt_txt);
+                    $("#forecastday3").text(response.list[2].dt_txt);
+                    $("#forecastday4").text(response.list[3].dt_txt);
+                    $("#forecastday5").text(response.list[4].dt_txt);
                 });
             });
 
