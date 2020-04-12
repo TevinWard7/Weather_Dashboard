82a8b69caeb735c83f66758ee1614845b5985bdc
Tevin <tevinward7@gmail.com>
2020-04-11 22:38:53 -0400

    fixed some bugs

--- script.js
+++ script.js
@@ -21,6 +21,12 @@ $(document).ready(function () {
             $("#humidity").text(response.main.humidity);
             $("#wind-speed").text(response.wind.speed);
 
+            // Append To List Of Searches
+            var listed = $("<li>");
+            listed.text(response.name);
+            listed.addClass("collection-item")
+            $(".collection").prepend(listed);
+
             var lon = response.coord.lon;
             var lat = response.coord.lat;
             var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
@@ -33,7 +39,7 @@ $(document).ready(function () {
                 // Display UV Index
                 $("#uv-index").text(response.value);
 
-                // Conditions To Add Css Styling
+                // Conditions To Add CSS Styling
                 if (response.value < 5) {
                     $("#uv-index").addClass("uv-ok")
                 }
@@ -74,17 +80,8 @@ $(document).ready(function () {
                     $("#forecast-humid4").text(response.list[3].main.humidity);
                     $("#forecast-humid5").text(response.list[4].main.humidity);
 
-                    // Display Temperature Icon
-                    if (response.list[0].main.temp > 72 || response.list[1].main.temp > 72 || response.list[2].main.temp > 72 || response.list[3].main.temp > 72 || response.list[4].main.temp > 72) {
-                        $("#icon").attr("src", "https://icons.iconarchive.com/icons/custom-icon-design/weather/256/Sunny-icon.png");
-                    }
+                    checkConditions();
 
-
-                    // Append To List Of Searches
-                    var listed = $("<li>");
-                    listed.text(city);
-                    listed.addClass("collection-item")
-                    $(".collection").prepend(listed);
                 });
             });
 
@@ -165,78 +162,81 @@ $(document).ready(function () {
                     // console.log(response.list[0].weather[0].description);
 
 
-                    var daysArray = response.list
+                    function checkConditions() {
+                        var daysArray = response.list
 
-                    console.log(daysArray)
+                        console.log(daysArray)
 
-                    // Light Rain & Moderate Rain
-                    if (daysArray[0].weather[0].description === "light rain" || daysArray[0].weather[0].description === "moderate rain") {
-                        $("#icon-1").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
-                    }
-                    if (daysArray[1].weather[0].description === "light rain" || daysArray[1].weather[0].description === "moderate rain") {
-                        $("#icon-2").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
-                    }
-                    if (daysArray[2].weather[0].description === "light rain" || daysArray[2].weather[0].description === "moderate rain") {
-                        $("#icon-3").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
-                    }
-                    if (daysArray[3].weather[0].description === "light rain" || daysArray[3].weather[0].description === "moderate rain") {
-                        $("#icon-4").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
-                    }
-                    if (daysArray[4].weather[0].description === "light rain" || daysArray[4].weather[0].description === "moderate rain") {
-                        $("#icon-3").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
+                        // Light Rain & Moderate Rain
+                        if (daysArray[0].weather[0].description === "light rain" || daysArray[0].weather[0].description === "moderate rain") {
+                            $("#icon-1").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
+                        }
+                        if (daysArray[1].weather[0].description === "light rain" || daysArray[1].weather[0].description === "moderate rain") {
+                            $("#icon-2").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
+                        }
+                        if (daysArray[2].weather[0].description === "light rain" || daysArray[2].weather[0].description === "moderate rain") {
+                            $("#icon-3").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
+                        }
+                        if (daysArray[3].weather[0].description === "light rain" || daysArray[3].weather[0].description === "moderate rain") {
+                            $("#icon-4").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
+                        }
+                        if (daysArray[4].weather[0].description === "light rain" || daysArray[4].weather[0].description === "moderate rain") {
+                            $("#icon-3").attr("src", "images/kisspng-emoji-cloud-rain-sky-computer-icons-5b37b2c14ec929.4957995615303768973227.png")
+                        }
+
+                        // Clear Sky
+                        if (daysArray[0].weather[0].description === "clear sky") {
+                            $("#icon-1").attr("src", "images/clear-sky.png")
+                        }
+                        if (daysArray[1].weather[0].description === "clear sky") {
+                            $("#icon-2").attr("src", "images/clear-sky.png")
+                        }
+                        if (daysArray[2].weather[0].description === "clear sky") {
+                            $("#icon-3").attr("src", "images/clear-sky.png")
+                        }
+                        if (daysArray[3].weather[0].description === "clear sky") {
+                            $("#icon-4").attr("src", "images/clear-sky.png")
+                        }
+                        if (daysArray[4].weather[0].description === "clear sky") {
+                            $("#icon-3").attr("src", "images/clear-sky.png")
+                        }
+
+                        // Overcast Clouds
+                        if (daysArray[0].weather[0].description === "overcast clouds") {
+                            $("#icon-1").attr("src", "images/overcast.png")
+                        }
+                        if (daysArray[1].weather[0].description === "overcast clouds") {
+                            $("#icon-2").attr("src", "images/overcast.png")
+                        }
+                        if (daysArray[2].weather[0].description === "overcast clouds") {
+                            $("#icon-3").attr("src", "images/overcast.png")
+                        }
+                        if (daysArray[3].weather[0].description === "overcast clouds") {
+                            $("#icon-4").attr("src", "images/overcast.png")
+                        }
+                        if (daysArray[4].weather[0].description === "overcast clouds") {
+                            $("#icon-3").attr("src", "images/overcast.png")
+                        }
+
+                        // Broken Clouds & Few Clouds & Scattered Clouds
+                        if (daysArray[0].weather[0].description === "broken clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
+                            $("#icon-1").attr("src", "images/cloud.png")
+                        }
+                        if (daysArray[1].weather[0].description === "broken clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
+                            $("#icon-2").attr("src", "images/cloud.png")
+                        }
+                        if (daysArray[2].weather[0].description === "overcast clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
+                            $("#icon-3").attr("src", "images/cloud.png")
+                        }
+                        if (daysArray[3].weather[0].description === "broken clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
+                            $("#icon-4").attr("src", "images/cloud.png")
+                        }
+                        if (daysArray[4].weather[0].description === "broken clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
+                            $("#icon-3").attr("src", "images/cloud.png")
+                        }
                     }
 
-                    // Clear Sky
-                    if (daysArray[0].weather[0].description === "clear sky") {
-                        $("#icon-1").attr("src", "images/pngguru.com.png")
-                    }
-                    if (daysArray[1].weather[0].description === "clear sky") {
-                        $("#icon-2").attr("src", "images/pngguru.com.png")
-                    }
-                    if (daysArray[2].weather[0].description === "clear sky") {
-                        $("#icon-3").attr("src", "images/pngguru.com.png")
-                    }
-                    if (daysArray[3].weather[0].description === "clear sky") {
-                        $("#icon-4").attr("src", "images/pngguru.com.png")
-                    }
-                    if (daysArray[4].weather[0].description === "clear sky") {
-                        $("#icon-3").attr("src", "images/pngguru.com.png")
-                    }
-
-                    // Overcast Clouds
-                    if (daysArray[0].weather[0].description === "overcast clouds") {
-                        $("#icon-1").attr("src", "images/PngItem_947417.png")
-                    }
-                    if (daysArray[1].weather[0].description === "overcast clouds") {
-                        $("#icon-2").attr("src", "images/PngItem_947417.png")
-                    }
-                    if (daysArray[2].weather[0].description === "overcast clouds") {
-                        $("#icon-3").attr("src", "images/PngItem_947417.png")
-                    }
-                    if (daysArray[3].weather[0].description === "overcast clouds") {
-                        $("#icon-4").attr("src", "images/PngItem_947417.png")
-                    }
-                    if (daysArray[4].weather[0].description === "overcast clouds") {
-                        $("#icon-3").attr("src", "images/PngItem_947417.png")
-                    }
-
-                    // Broken Clouds & Few Clouds & Scattered Clouds
-                    if (daysArray[0].weather[0].description === "broken clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
-                        $("#icon-1").attr("src", "images/hiclipart.com.png")
-                    }
-                    if (daysArray[1].weather[0].description === "broken clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
-                        $("#icon-2").attr("src", "images/hiclipart.com.pngg")
-                    }
-                    if (daysArray[2].weather[0].description === "overcast clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
-                        $("#icon-3").attr("src", "images/hiclipart.com.png")
-                    }
-                    if (daysArray[3].weather[0].description === "broken clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
-                        $("#icon-4").attr("src", "images/hiclipart.com.png")
-                    }
-                    if (daysArray[4].weather[0].description === "broken clouds" || daysArray[0].weather[0].description === "few clouds" || daysArray[0].weather[0].description === "scattered clouds") {
-                        $("#icon-3").attr("src", "images/hiclipart.com.png")
-                    }
-
+                    checkConditions()
                 });
             });
 
